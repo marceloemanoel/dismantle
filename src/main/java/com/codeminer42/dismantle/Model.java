29e799022ef5681d6f98c6d4608d1a630d50791f
Eduardo Gurgel <eduardo.gurgel@codeminer42.com>
2013-01-31 11:44:33 -0300

    Add externalRepresentation method

--- src/main/java/com/codeminer42/dismantle/Model.java
+++ src/main/java/com/codeminer42/dismantle/Model.java
@@ -3,20 +3,34 @@ package com.codeminer42.dismantle;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Model {
     abstract protected Map<String, String> externalRepresentationKeyPaths();
 
-    protected Model(Map<String, String> externalRepresentation) {
+    protected Model() {
+
+    }
+
+    protected Model(Map<String, Object> externalRepresentation) {
         Map<String, String> selfRepresentation = this.externalRepresentationKeyPaths();
         for (String property : selfRepresentation.keySet()) {
-            String transformation = externalRepresentation.get(selfRepresentation.get(property));
-            assignProperty(property, transformation);
+            Object transformable = externalRepresentation.get(selfRepresentation.get(property));
+            assignProperty(property, transformable);
         }
     }
 
-    private void assignProperty(String property, String mapValue) {
+    public Map<String, Object> externalRepresentation() {
+        Map<String, String> selfRepresentation = this.externalRepresentationKeyPaths();
+        Map<String, Object> representation = new HashMap<String, Object>();
+        for (String property : selfRepresentation.keySet()) {
+            representation.put(property, tryToGetField(property));
+        }
+        return representation;
+    }
+
+    private void assignProperty(String property, Object mapValue) {
         Object result = null;
         try {
             try {
@@ -34,15 +48,13 @@ public abstract class Model {
         }
     }
 
-    private Object tryToInvokeTransformation(String property, String mapValue) throws NoSuchMethodException {
+    private Object tryToInvokeTransformation(String property, Object mapValue) throws NoSuchMethodException {
         try {
             Method method = this.getClass().getMethod("transformTo" + property.substring(0, 1).toUpperCase() + property.substring(1), Object.class);
             return method.invoke(this, mapValue);
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (NullPointerException e ) {
             e.printStackTrace();
         }
         return null;
@@ -58,5 +70,16 @@ public abstract class Model {
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    private Object tryToGetField(String property) {
+        try {
+            Field field = this.getClass().getDeclaredField(property);
+            field.setAccessible(true);
+            return field.get(this);
+        } catch (NoSuchFieldException e) {
+        } catch (IllegalAccessException e) {
+        }
+        return null;
     }
 }
--- src/test/java/com/codeminer42/dismantle/ModelTest.java
+++ src/test/java/com/codeminer42/dismantle/ModelTest.java
@@ -18,7 +18,11 @@ public class ModelTest {
         public Double distance;
         public String content;
 
-        public ModelExample(Map<String, String> rep) {
+        public ModelExample() {
+            super();
+        }
+
+        public ModelExample(Map<String, Object> rep) {
             super(rep);
         }
 
@@ -46,7 +50,7 @@ public class ModelTest {
 
     @Test
     public void testPropertiesWithTransformMethods() {
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<String, Object>();
         map.put("birth_date", "2002");
         map.put("distance", "2000.0");
         ModelExample example = new ModelExample(map);
@@ -56,11 +60,37 @@ public class ModelTest {
 
     @Test
     public void testPropertiesWithoutTransformMethods() {
-        Map<String, String> map = new HashMap<String, String>();
+        Map<String, Object> map = new HashMap<String, Object>();
         map.put("CONTENT", "Codeminer 42");
         ModelExample example = new ModelExample(map);
         assertThat(example.content, is("Codeminer 42"));
         assertThat(example.distance, nullValue());
         assertThat(example.birthdate, nullValue());
+    }
+
+    @Test
+    public void testExternalRepresentationWithCompleteExample() {
+        ModelExample example = new ModelExample();
+        example.birthdate = "2002";
+        example.distance = 200.9;
+        example.content = "Codeminer 42";
+
+        Map<String, Object> rep = example.externalRepresentation();
+
+        assertThat((String) rep.get("birthdate"), is("2002"));
+        assertThat((Double) rep.get("distance"), is(200.9));
+        assertThat((String) rep.get("content"), is("Codeminer 42"));
+    }
+
+    @Test
+    public void testExternalRepresentationWithIncompleteExample() {
+        ModelExample example = new ModelExample();
+        example.birthdate = "2002";
+
+        Map<String, Object> rep = example.externalRepresentation();
+
+        assertThat((String) rep.get("birthdate"), is("2002"));
+        assertThat((Double) rep.get("distance"), nullValue());
+        assertThat((String) rep.get("content"), nullValue());
     }
 }
