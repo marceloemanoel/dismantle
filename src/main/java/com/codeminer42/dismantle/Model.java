3ed21a915874aac553b73f309423a167ba5a454f
Eduardo Gurgel <eduardo.gurgel@codeminer42.com>
2013-02-01 13:30:26 -0300

    Minor refactor and add recursive getField.

--- src/main/java/com/codeminer42/dismantle/Model.java
+++ src/main/java/com/codeminer42/dismantle/Model.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 public abstract class Model {
-    abstract protected Map<String, String> externalRepresentationKeyPaths();
+    abstract public Map<String, String> externalRepresentationKeyPaths();
 
     protected Model() {
 
@@ -17,19 +17,19 @@ public abstract class Model {
         final Map<String, String> selfRepresentation = this.externalRepresentationKeyPaths();
         for (String property : selfRepresentation.keySet()) {
             String path = selfRepresentation.get(property);
-            Object transformable = getData(externalRepresentation, selfRepresentation, path);
+            Object transformable = getData(externalRepresentation, path);
             assignProperty(property, transformable);
         }
     }
 
-    private static Object getData(Map<String, Object> externalRepresentation, Map<String, String> selfRepresentation, String path) {
+    private static Object getData(Map<String, Object> externalRepresentation, String path) {
         if (path.contains(".")) {
             String[] subPaths = path.split("\\.");
             Object nestedObject = externalRepresentation.get(subPaths[0]);
             if (nestedObject instanceof Map) {
                 try {
                     Map<String, Object> nestedRepresentation = (Map<String, Object>) nestedObject;
-                    return getData(nestedRepresentation, selfRepresentation, path.substring(subPaths[0].length()+1));
+                    return getData(nestedRepresentation, path.substring(subPaths[0].length()+1));
                 } catch (ClassCastException e) {
                     // Well, not a Map<String, Object> :/
                     e.printStackTrace();
@@ -70,7 +70,7 @@ public abstract class Model {
 
     private Object getProperty(String property) {
         Object propertyData = tryToGetField(property);
-        Object result = null;
+        Object result;
         try {
             result = tryToInvokeTransformationFrom(property, propertyData);
         } catch (NoSuchMethodException e) {
@@ -79,52 +79,68 @@ public abstract class Model {
         return result;
     }
 
-    private final Object tryToInvokeTransformationTo(String property, Object mapValue) throws NoSuchMethodException {
+    private Object tryToInvokeTransformationTo(String property, Object mapValue) throws NoSuchMethodException {
         try {
             Method method = this.getClass().getDeclaredMethod("transformTo" + property.substring(0, 1).toUpperCase() + property.substring(1), Object.class);
             method.setAccessible(true);
             return method.invoke(this, mapValue);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        } catch (InvocationTargetException ignored) {
+        } catch (IllegalAccessException ignored) {
         }
         return null;
     }
 
-    private final Object tryToInvokeTransformationFrom(String property, Object propertyValue) throws NoSuchMethodException {
+    private Object tryToInvokeTransformationFrom(String property, Object propertyValue) throws NoSuchMethodException {
         try {
-            Method method = this.getClass().getDeclaredMethod("transformFrom" + property.substring(0, 1).toUpperCase() + property.substring(1), Object.class);
+            Method method = this.getClass().getDeclaredMethod("transformFrom" + captitalizeFirstLetter(property), Object.class);
             method.setAccessible(true);
             return method.invoke(this, propertyValue);
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        } catch (InvocationTargetException ignored) {
+        } catch (IllegalAccessException ignored) {
         }
         return null;
     }
 
     private void tryToSetField(String property, Object data) {
         try {
-            Field field = this.getClass().getDeclaredField(property);
+            Field field = getField(this.getClass(), property);
             field.setAccessible(true);
             field.set(this, data);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
+        } catch (NoSuchFieldException ignored) {
+        } catch (IllegalAccessException ignored) {
         }
     }
 
+    /**
+     * Get the field through the super classes
+     * @param property
+     * @return property data
+     */
     private final Object tryToGetField(String property) {
         try {
-            Field field = this.getClass().getDeclaredField(property);
+            Field field = getField(this.getClass(), property);
             field.setAccessible(true);
             return field.get(this);
-        } catch (NoSuchFieldException e) {
-        } catch (IllegalAccessException e) {
+        } catch (NoSuchFieldException ignored) {
+        } catch (IllegalAccessException ignored) {
         }
         return null;
+    }
+
+    private static String captitalizeFirstLetter(String word) {
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+
+    private static Field getField(Class clazz, String fieldName) throws NoSuchFieldException {
+        try {
+            return clazz.getDeclaredField(fieldName);
+        } catch (NoSuchFieldException e) {
+            Class superClass = clazz.getSuperclass();
+            if (superClass == null) {
+                throw e;
+            } else {
+                return getField(superClass, fieldName);
+            }
+        }
     }
 }
